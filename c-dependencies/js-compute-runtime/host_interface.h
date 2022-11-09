14bf530e2d775c59eadefd37ad37aab856c6181f
Guy Bedford <gbedford@fastly.com>
2022-11-09 15:39:14 -0800

    convert_result

--- c-dependencies/js-compute-runtime/host_interface.h
+++ c-dependencies/js-compute-runtime/host_interface.h
@@ -2,7 +2,7 @@
 #define HOST_INTERFACE_H
 
 #include "xqd_remaining.h"
-#include "executed/executed.h"
+#include "executed.h"
 
 static executed_fastly_status_t fastly_abi_init(uint64_t abi_version);
 
@@ -12,11 +12,15 @@ static inline executed_fastly_status_t fastly_abi_init(uint64_t abi_version) {
     return executed_fastly_abi_init(abi_version);
 }
 
+static inline executed_fastly_status_t fastly_http_req_new(uint32_t *ret) {
+    return executed_fastly_http_req_new(ret);
+}
+
 #else
 
 #include "xqd_migrated.h"
 
-static inline int convert_result(executed_fastly_status_t res) {
+static inline int convert_result(int res) {
     switch (res) {
         case 0:
             return EXECUTED_FASTLY_STATUS_OK;
@@ -32,6 +36,13 @@ static inline int convert_result(executed_fastly_status_t res) {
 
 static inline executed_fastly_status_t fastly_abi_init(uint64_t abi_version) {
     return convert_result(xqd_init(abi_version));
+}
+
+static inline executed_fastly_status_t fastly_http_req_new(uint32_t *ret) {
+    RequestHandle request_handle = {INVALID_HANDLE};
+    int res = convert_result(xqd_req_new(&request_handle));
+    *ret = request_handle.handle;
+    return res;
 }
 
 #endif
