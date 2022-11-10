dd85d61a34f17374036a554babab1fa673b15411
Trevor Elliott <telliott@fastly.com>
2022-11-09 16:54:34 -0800

    Change to the host_interface directory in regenerate.sh

--- c-dependencies/js-compute-runtime/host_interface/regenerate.sh
+++ c-dependencies/js-compute-runtime/host_interface/regenerate.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 WIT_BINDGEN="${WIT_BINDGEN:-$HOME/src/bytecodealliance/wit-bindgen/target/release/wit-bindgen}"
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 "$WIT_BINDGEN" guest c \
   --import executed.wit \
   --out-dir . \
